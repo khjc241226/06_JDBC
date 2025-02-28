1fb9240cf9927130de014351b7e9234c65e8e08d
khjc241226 <khjc241226@gmail.com>
2025-02-28 11:18:51 +0900

    20250228_JDBC2

---
+++ jdbc2/src/example/JDBCExample4.java
@@ -0,0 +1,15 @@
+package example;
+
+public class JDBCExample4 {
+	public static void main(String[] args) {
+		
+		// 아이디, 비밀번호, 새 비밀번호를 입력 받아
+		// 아이디, 비밀번호가 일치하는 회원의 비밀번호 변경
+		
+		
+		
+		
+		
+		
+	}
+}
